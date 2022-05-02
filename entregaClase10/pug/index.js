ea20a98024c3710dd79e4f76b5519d0999aa4491
ferni232 <fernandorudnevich.inedita@gmail.com>
2022-05-02 00:14:29 -0300

    empece pug

--- entregaClase10/pug/index.js
+++ entregaClase10/pug/index.js
@@ -1,19 +1,23 @@
 const express = require("express");
-
+const productosRoutes = require("./api/productos");
 
 const app=express();
+//midelwars de aplicacion
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))//para decodificar la url .
 
 app.set("view engine","pug");
 app.set("views","./views");
 
-app.get("/",(req,res)=>{
+app.use("/api/productos",productosRoutes);
+/* app.get("/",(req,res)=>{
     res.render("index")
 })
 
 app.get("/pr",(req,res)=>{
     res.render("productos")
 })
-
+ */
 app.listen(8080,()=>{
     console.log("server ok")
 })
